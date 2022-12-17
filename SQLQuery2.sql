10dce569f9193cf263b1edbde9dff7cb6e8e575f
Yusuf S. Kaygusuz <yusufskaygusuz@gmail.com>
2022-12-17 13:03:35 +0300

    ProcedureWithView
    Show the total and average sales in the desired year on the basis of product or category according to the parameter given in a procedure using the money symbol

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -25,3 +25,58 @@ Set @counter +=1
 End
 
 Select @avgPrice AS last_avg_price, @counter AS Sayac
+
+
+--Show the total and average sales in the desired year on the basis of product or category according
+--to the parameter given in a procedure using the money symbol
+
+Go
+Create or alter view vwSiparisDetay As
+Select OrderID AS siparisID, ProductID As UrunID, 
+Quantity*UnitPrice - OD.Discount AS NetTutar
+From [Order Details] OD
+Go
+Select * from dbo.vwSiparisDetay
+Go
+
+Create or alter Procedure spUrunKategoriSatislari (@yil int, @sorguNo int = 1)
+As
+Set Nocount on
+If (@sorguNo = 1)
+Begin
+	Select P.ProductName,
+	Format(Sum(SD.NetTutar), 'c', 'en-us') AS totalSelling,
+	FORMAT(AVG(SD.NetTutar), 'c', 'en-us') AS averageSelling
+
+	From Orders O 
+	inner join vwSiparisDetay SD ON SD.siparisID = O.OrderID
+	inner join Products P ON P.ProductID = SD.UrunID
+
+	Where Year(O.OrderDate) = @yil
+
+	Group By p.ProductName
+	Order BY P.ProductName
+End
+Else if (@sorguNo = 2)
+Begin
+	Select C.CategoryName,
+	Format(Sum(SD.NetTutar), 'c', 'en-us') AS totalCSelling,
+	Format(AVG(SD.NetTutar), 'c', 'en-us') AS averageCSelling
+
+	From Orders O 
+	inner join vwSiparisDetay SD ON SD.siparisID = O.OrderID
+	inner join Products P ON P.ProductID = SD.UrunID
+	inner join Categories C ON C.CategoryID = P.CategoryID
+
+	Where Year(O.OrderDate) = @yil
+	
+	Group By C.CategoryName
+	Order By C.CategoryName
+End
+Set nocount off
+Go
+
+Exec spUrunKategoriSatislari 1997,1
+Exec spUrunKategoriSatislari 1997,2
+Go
+
